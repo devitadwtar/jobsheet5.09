ac4634bc67e77ba8418bbea9e400e9a3f472cf1c
devitadwtar <144086102+devitadwtar@users.noreply.github.com>
2023-09-26 12:58:10 +0700

    Add files via upload

---
+++ PemilihanPercobaan109_1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class PemilihanPercobaan109_1{
+    public static void main(String[] args){
+
+        Scanner input09 = new Scanner(System.in);
+
+        System.out.print("Masukkan angka; ");
+        int Nilai = input09.nextInt();
+
+        if (Nilai < 75) {
+            System.out.println("Anda harus remidi");
+        }
+        else {
+            System.out.println("Anda lulus");
+        }   
+    }
+}
